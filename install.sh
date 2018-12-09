ae6f88b948f14f8edd5e228977f997af47ec5d44
Pieter Jan <pjgeutjens@centos7.leap.local>
2018-12-09 11:14:46 +0100

    add installer

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# install packages
+
+sudo yum install -y i3 terminator
+
+# install fonts
+
+./fonts/install.sh
+
+# copy config for terminator and i3
+echo "copying terminator and i3 configs" 
+cp -rf .config/terminator $HOME
+cp -rf .config/i3 $HOME
+
+# copy vim settings
+
+echo "copying .vim folder"
+cp -rf .vim $HOME
+echo "copying .vimrc"
+cp -f .vimrc $HOME
