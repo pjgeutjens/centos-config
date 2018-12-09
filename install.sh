88418867f483bfb1dee7e0e7c4668fa3ee17a2cc
Pieter Jan <pjgeutjens@centos7.leap.local>
2018-12-09 11:16:23 +0100

    remove empty lines

--- install.sh
+++ install.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 # install packages
-
 sudo yum install -y i3 terminator
 
 # install fonts
-
 ./fonts/install.sh
 
 # copy config for terminator and i3
@@ -14,7 +12,6 @@ cp -rf .config/terminator $HOME
 cp -rf .config/i3 $HOME
 
 # copy vim settings
-
 echo "copying .vim folder"
 cp -rf .vim $HOME
 echo "copying .vimrc"
