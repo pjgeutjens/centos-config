69579520acbd90a63e1ecad13228000e5420f7ab
Pieter Jan <pjgeutjens@centos-dev.mgnt.belbone.be>
2018-12-20 11:11:07 +0100

    chrome stable

--- packages/google-chrome.sh
+++ packages/google-chrome.sh
@@ -5,4 +5,4 @@ sudo rpm --import https://dl.google.com/linux/linux_signing_key.pub
 sudo sh -c 'echo -e "[google-chrome]\nname=Google Chrome\nbaseurl=http://dl.google.com/linux/chrome/rpm/stable/x86_64\nenabled=1\ngpgcheck=1\ngpgkey=https://dl.google.com/linux/linux_signing_key.pub" > /etc/yum.repos.d/google-chrome.repo'
 echo "Installing..."
 yum check-update
-sudo yum install google-chrome
+sudo yum install google-chrome-stable
